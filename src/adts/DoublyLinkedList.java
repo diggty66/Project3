b5652cf3ace8417c38ad64a45992de32dc531367
ryanhassmiller <72896287+ryanhassmiller@users.noreply.github.com>
2020-11-04 20:34:55 -0500

    Add files via upload

---
+++ src/adts/DoublyLinkedList.java
@@ -0,0 +1,211 @@
+package adts;
+
+import java.util.Arrays;
+import interfaces.ListInterface;
+import nodes.DLLNode;
+
+public class DoublyLinkedList<E> implements ListInterface<E> {
+
+		protected DLLNode<E> front;
+		protected DLLNode<E> rear;
+
+		protected int numElements;
+		protected int curIteratorPos;
+		//for find methods
+		protected boolean found;
+		protected boolean called;
+
+		protected DLLNode<E> location;
+	@Override
+	public void add(E element) {
+		DLLNode<E> newNode = new DLLNode(element);
+		called = true;
+		if(front==null && rear == null) {
+			front = newNode;
+			rear=front;
+			
+		}else {
+			
+			newNode.setPrevious(rear);
+			rear.setNext(newNode);
+			rear = newNode;
+			
+			/*rear.setPrevious(front);
+			rear.setNext(newNode);
+			rear=newNode;*/
+
+		}
+		numElements++;
+	}
+	protected void find2(E target){
+		found = false;
+		E[] list = null;
+		DLLNode<E> current = front;
+
+		if(called){
+			called = false;
+			list = (E[]) new Object[numElements];
+			for(int i = 0; i < numElements; ++i){
+				list[i] = current.getInfo();
+				current.getNext();
+			}
+			Arrays.sort(list);
+		}
+		
+		
+		int index = 0, length = numElements - 1;
+		while (index <= length){
+			int mid = index + (length - index)/2;
+
+			if(list[mid] == target){
+				location.setInfo(list[mid]);
+				found = true;
+				return;
+			}
+			
+			if (list[mid].toString().compareTo(target.toString()) > 0){
+				length = mid - 1;
+			}
+			else {
+				index = mid + 1;
+			}
+		}
+	}
+
+	public String compareFind(E target){
+		
+		long startTime = System.nanoTime();
+		find(target);
+		long elapsed = System.nanoTime() - startTime;
+
+		String answer = "The time in nanoseconds for linear search: " + elapsed; 
+
+		startTime = System.nanoTime();
+		find2(target);
+		elapsed = System.nanoTime() - startTime;
+
+		answer += "\nThe time in nanoseconds for binary search: " + elapsed;
+
+		return answer;
+	}
+
+	protected void find(E target) {
+
+		DLLNode<E> current = front;
+
+		found = false;
+		
+		while(curIteratorPos<numElements) {
+			if(current.getInfo().equals(target)) {
+
+				location = current;
+				found=true;
+				
+				return;
+			}else {
+				current = current.getNext();
+
+				curIteratorPos++;
+			}
+		}
+	}
+
+	@Override
+	public boolean remove(E element) {
+		called = true;
+		
+		find(element);
+			if(found) {
+				if(front == rear) {
+					front = rear = null;
+				}
+				else if(front == location) {
+					front = front.getNext();
+					front.setPrevious(null);
+				}
+				else if(rear == location){
+						rear = rear.getPrevious();
+						rear.setNext(null);
+				}
+				else {
+					location.getPrevious().setNext(location.getNext());
+					location.getNext().setPrevious(location.getPrevious());
+				}
+			}
+
+		resetIterator();
+		numElements--;
+		return false;	
+	}
+
+
+
+	@Override
+	public int size() {
+		return numElements;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return front==null;
+	}
+
+	@Override
+	public boolean contains(E element) {
+		find(element);
+		return found;
+	}
+
+	@Override
+	public E get(E element) {
+		find(element);
+		if(found){
+			return location.getInfo();
+		}
+		else{
+			return null;
+		}
+	}
+
+	@Override
+	public void resetIterator() {
+		curIteratorPos=0;
+	}
+	
+	public void resetBackIterator() {
+		curIteratorPos=numElements-1;
+	}
+	
+	public E getPrevItem() {
+		resetBackIterator();
+		DLLNode<E> back = location.getPrevious();
+		curIteratorPos--;
+		if(curIteratorPos==0) {
+			resetBackIterator();
+		}
+		return back.getInfo();
+		
+	}
+
+	@Override
+	public E getNextItem() {
+		DLLNode<E> next = location.getNext();
+		++curIteratorPos;
+		if (curIteratorPos == numElements-1) {
+	    	curIteratorPos = 0;
+	    }
+	    return next.getInfo();
+		
+	}
+	
+	public String toString() {
+		String str = "\n-----------------\n";
+		DLLNode<E> current = front;
+		while(current!=null) {
+			str = str + current.getInfo() + " ";
+			current = current.getNext();
+		}
+		return str;
+	}
+
+}
